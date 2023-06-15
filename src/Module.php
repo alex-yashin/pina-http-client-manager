b95e05c312e323438b81707c9c98f4c5b7553561
Alex Yashin <alex-yashin@ya.ru>
2023-06-15 11:38:11 +0400

    автоматически не регистрируем эндпоинты и не выдаем на них права

--- src/Module.php
+++ src/Module.php
@@ -3,10 +3,6 @@
 
 namespace PinaHttpClientManager;
 
-
-use Exception;
-use Pina\Access;
-use Pina\App;
 use Pina\ModuleInterface;
 
 class Module implements ModuleInterface
@@ -26,21 +22,9 @@ class Module implements ModuleInterface
         return 'Clients';
     }
 
-    /**
-     * @return array
-     * @throws Exception
-     */
     public function http()
     {
-        return $this->initRouter();
-    }
-
-    public function initRouter()
-    {
-        App::router()->register('clients', Endpoints\ClientEndpoint::class);
-        App::router()->register('clients/:client_id/webooks', Endpoints\WebhookEndpoint::class, ['client_id']);
-
-        Access::permit('clients', 'registered');
         return [];
     }
+
 }
