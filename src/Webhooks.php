788ad69b9c414f1fb4d0ed52a99b94f2e8e78046
Alex Yashin <alex-yashin@ya.ru>
2025-12-04 15:56:49 +0400

    fix

--- src/Webhooks.php
+++ src/Webhooks.php
@@ -2,7 +2,6 @@
 
 namespace PinaHttpClientManager;
 
-use Pina\Events\QueueableCommand;
 use PinaHttpClientManager\Model\WebhookMessage;
 use PinaHttpClientManager\SQL\WebhookGateway;
 
@@ -17,8 +16,7 @@ class Webhooks
 
         foreach ($subscribers as $clientId) {
             //запускаем в фоновом режиме с помощью доступного сервера очередей
-            $command = new QueueableCommand(Commands\Notify::class);
-            $command($message->getPacket($clientId));
+            Commands\Notify::enqueue($clientId);
         }
     }
 
